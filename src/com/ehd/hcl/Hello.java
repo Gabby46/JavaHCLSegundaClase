383d1e093677a58b3136477fe66d13fcf771a017
Eduardo Herrera <eherrerad94@gmail.com>
2017-08-15 16:01:19 -0500

    Hello class

---
+++ src/com/ehd/hcl/Hello.java
@@ -0,0 +1,15 @@
+package com.ehd.hcl;
+
+public class Hello {
+
+	public Hello() {
+		System.out.println("Hello world from Constructor");
+	}
+
+	public static void main(String[] args) {
+		new Hello();
+		
+
+	}
+
+}
